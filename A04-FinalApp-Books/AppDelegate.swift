8b66f411f88b64dd06dc9daee72817d45bf201f1
Obliviann <34093004+Obliviann@users.noreply.github.com>
2020-02-12 15:00:05 +0100

    init Firebase in AppDelegate and ViewController + added Google Info.plist

--- A04-FinalApp-Books/AppDelegate.swift
+++ A04-FinalApp-Books/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        //2.Recomendado poner dentro del didFinishLaunching, que lo ejecuta después de que la app arranque
+        //2.Llamamos a la func del DataHolder donde pusimos la segunda línea de inic de Firebase
+        DataHolder.sharedInstance.initFirebase()
+
         return true
     }
 
